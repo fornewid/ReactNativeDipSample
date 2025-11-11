d76268c52e9e5f10a8f563e5085b83e508df6ee5
Sungyong An <sungyong.an@webtoonscorp.com>
2025-11-11 23:38:23 +0900

    (#6) Image

--- app/index.tsx
+++ app/index.tsx
@@ -1,41 +1,42 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, StyleSheet } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
-const LimitedStyleInheritance = () => (
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  logo: {
+    width: 66,
+    height: 58,
+  },
+});
+
+const DisplayAnImage = () => (
   <SafeAreaProvider>
-    <SafeAreaView>
-      <View>
-        <Text>
-          Some text
-        </Text>
-      </View>
-      <View>
-        <MyAppText>
-          Text styled with the default font for the entire application
-        </MyAppText>
-        <MyAppHeaderText>Text styled as a header</MyAppHeaderText>
-      </View>
-      <Text style={{fontWeight: 'bold'}}>
-        I am bold
-        <Text style={{color: 'red'}}> and red</Text>  {/* bold + red */}
-      </Text>
+    <SafeAreaView style={styles.container}>
+      <Image
+        style={styles.tinyLogo}
+        source={require('@expo/snack-static/react-native-logo.png')}
+      />
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: 'https://reactnative.dev/img/tiny_logo.png',
+        }}
+      />
+      <Image
+        style={styles.logo}
+        source={{
+          uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
+        }}
+      />
     </SafeAreaView>
   </SafeAreaProvider>
 );
 
-const MyAppHeaderText = ({children}) => {
-  return (
-    <MyAppText>
-      <Text style={{fontSize: 20}}>{children}</Text>
-    </MyAppText>
-  );
-};
-
-const MyAppText = ({children}) => {
-  return (
-    <Text style={{color: 'green', fontStyle: 'italic'}}>{children}</Text>
-  );
-};
-
-export default LimitedStyleInheritance;
+export default DisplayAnImage;
