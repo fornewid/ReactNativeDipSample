9d0376d5874f89e086163cfdc2ec55c0ad3b8a70
Sungyong An <sungyong.an@webtoonscorp.com>
2025-11-11 23:02:25 +0900

    (#3) Props

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
 
-type CatProps = {
-  name: string;
-};
-
-const Cat = (props: CatProps) => {
+const CatApp = () => {
   return (
     <View>
-      <Text>Hello, I am {props.name}!</Text>
+      <Image
+        source={{
+          uri: 'https://reactnative.dev/docs/assets/p_cat1.png',
+        }}
+        style={{width: 200, height: 200}}
+      />
+      <Text>Hello, I am your cat!</Text>
     </View>
   );
 };
 
-const Cafe = () => {
-  return (
-    <View>
-      <Cat name="Maru" />
-      <Cat name="Jellylorum" />
-      <Cat name="Spot" />
-    </View>
-  );
-};
-
-export default Cafe;
+export default CatApp;
