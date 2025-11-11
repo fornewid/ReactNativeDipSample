c43d4be845929095d6195893da6df6625fc79140
Sungyong An <sungyong.an@webtoonscorp.com>
2025-11-11 23:10:58 +0900

    (#5) Text: Nested text

--- app/index.tsx
+++ app/index.tsx
@@ -1,42 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { StyleSheet, Text } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
-const TextInANest = () => {
-  const [titleText, setTitleText] = useState("Bird's Nest");
-  const bodyText = 'This is not really a bird nest.';
-
-  const onPressTitle = () => {
-    setTitleText("Bird's Nest [pressed]");
-  };
-
-  return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.baseText}>
-          <Text style={styles.titleText} onPress={onPressTitle}>
-            {titleText}
-            {'\n'}
-            {'\n'}
-          </Text>
-          <Text numberOfLines={5}>{bodyText}</Text>
-        </Text>
-      </SafeAreaView>
-    </SafeAreaProvider>
-  );
-};
+const BoldAndBeautiful = () => (
+  <SafeAreaProvider>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.baseText}>
+        I am bold
+        <Text style={styles.innerText}> and red</Text>
+      </Text>
+    </SafeAreaView>
+  </SafeAreaProvider>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
   baseText: {
-    fontFamily: 'Cochin',
-  },
-  titleText: {
-    fontSize: 20,
     fontWeight: 'bold',
+  },
+  innerText: {
+    color: 'red',
   },
 });
 
-export default TextInANest;
+export default BoldAndBeautiful;
