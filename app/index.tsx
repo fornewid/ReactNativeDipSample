dedbd4ed14efc02c075bf00bd4cab9251712bbba
Sungyong An <sungyong.an@webtoonscorp.com>
2025-11-11 23:10:13 +0900

    (#5) Text

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,42 @@
-import React from 'react';
-import { Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
-const ViewBoxesWithColorAndText = () => {
+const TextInANest = () => {
+  const [titleText, setTitleText] = useState("Bird's Nest");
+  const bodyText = 'This is not really a bird nest.';
+
+  const onPressTitle = () => {
+    setTitleText("Bird's Nest [pressed]");
+  };
+
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{flexDirection: 'row'}}>
-        <View style={{height: 100, backgroundColor: 'blue', flex: 0.2}} />
-        <View style={{height: 100, backgroundColor: 'red', flex: 0.4}} />
-        <Text>Hello World!</Text>
-        <View>
-          <Text nativeID="formLabel">Label for Input Field</Text>
-          <TextInput aria-label="input" aria-labelledby="formLabel" />
-        </View>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.baseText}>
+          <Text style={styles.titleText} onPress={onPressTitle}>
+            {titleText}
+            {'\n'}
+            {'\n'}
+          </Text>
+          <Text numberOfLines={5}>{bodyText}</Text>
+        </Text>
       </SafeAreaView>
     </SafeAreaProvider>
   );
 };
 
-export default ViewBoxesWithColorAndText;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  baseText: {
+    fontFamily: 'Cochin',
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
+
+export default TextInANest;
