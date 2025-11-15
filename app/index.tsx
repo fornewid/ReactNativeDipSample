a502d3b6b08c62622ee0a909eee642c36837c031
Sungyong An <sungyong.an@webtoonscorp.com>
2025-11-15 15:14:59 +0900

    (#9) Style

--- app/index.tsx
+++ app/index.tsx
@@ -1,134 +1,29 @@
-import type { PropsWithChildren } from 'react';
-import React, { useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-const PositionLayout = () => {
-  const [position, setPosition] = useState<'relative' | 'absolute' | 'static'>(
-    'relative',
-  );
-
+const LotsOfStyles = () => {
   return (
-    <PreviewLayout
-      label="position"
-      selectedValue={position}
-      values={['relative', 'absolute', 'static']}
-      setSelectedValue={setPosition}>
-      <View
-        style={[
-          styles.box,
-          {
-            top: 25,
-            left: 25,
-            position,
-            backgroundColor: 'powderblue',
-          },
-        ]}
-      />
-      <View
-        style={[
-          styles.box,
-          {
-            top: 50,
-            left: 50,
-            position,
-            backgroundColor: 'skyblue',
-          },
-        ]}
-      />
-      <View
-        style={[
-          styles.box,
-          {
-            top: 75,
-            left: 75,
-            position,
-            backgroundColor: 'steelblue',
-          },
-        ]}
-      />
-    </PreviewLayout>
+    <View style={styles.container}>
+      <Text style={styles.red}>just red</Text>
+      <Text style={styles.bigBlue}>just bigBlue</Text>
+      <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
+      <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
+    </View>
   );
 };
 
-type PreviewLayoutProps = PropsWithChildren<{
-  label: string;
-  values: Array<'relative' | 'absolute' | 'static'>;
-  selectedValue: string;
-  setSelectedValue: (value: 'relative' | 'absolute' | 'static') => void;
-}>;
-
-const PreviewLayout = ({
-  label,
-  children,
-  values,
-  selectedValue,
-  setSelectedValue,
-}: PreviewLayoutProps) => (
-  <View style={{padding: 10, flex: 1}}>
-    <Text style={styles.label}>{label}</Text>
-    <View style={styles.row}>
-      {values.map(value => (
-        <TouchableOpacity
-          key={value}
-          onPress={() => setSelectedValue(value)}
-          style={[styles.button, selectedValue === value && styles.selected]}>
-          <Text
-            style={[
-              styles.buttonLabel,
-              selectedValue === value && styles.selectedLabel,
-            ]}>
-            {value}
-          </Text>
-        </TouchableOpacity>
-      ))}
-    </View>
-    <View style={styles.container}>{children}</View>
-  </View>
-);
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    marginTop: 8,
-    backgroundColor: 'aliceblue',
-    minHeight: 200,
+    marginTop: 50,
   },
-  box: {
-    width: 50,
-    height: 50,
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
   },
-  row: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-  },
-  button: {
-    paddingHorizontal: 8,
-    paddingVertical: 6,
-    borderRadius: 4,
-    backgroundColor: 'oldlace',
-    alignSelf: 'flex-start',
-    marginHorizontal: '1%',
-    marginBottom: 6,
-    minWidth: '48%',
-    textAlign: 'center',
-  },
-  selected: {
-    backgroundColor: 'coral',
-    borderWidth: 0,
-  },
-  buttonLabel: {
-    fontSize: 12,
-    fontWeight: '500',
-    color: 'coral',
-  },
-  selectedLabel: {
-    color: 'white',
-  },
-  label: {
-    textAlign: 'center',
-    marginBottom: 10,
-    fontSize: 24,
+  red: {
+    color: 'red',
   },
 });
 
-export default PositionLayout;
+export default LotsOfStyles;
