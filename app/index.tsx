e8d48ab9a86d81aa9911a7ae6337197e40b2756e
Sungyong An <sungyong.an@webtoonscorp.com>
2025-11-15 15:46:19 +0900

    (#12) TextInput

--- app/index.tsx
+++ app/index.tsx
@@ -1,35 +1,38 @@
 import React from 'react';
-import { Appearance, Button, Text, useColorScheme } from 'react-native';
+import { StyleSheet, TextInput } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
-const App = () => {
-  const colorScheme = useColorScheme();
+const TextInputExample = () => {
+  const [text, onChangeText] = React.useState('Useless Text');
+  const [number, onChangeNumber] = React.useState('');
+
   return (
     <SafeAreaProvider>
-      <SafeAreaView
-        style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: colorScheme === 'dark' ? '#000000' : '#FFFFFF',
-        }}>
-          <Text
-            style={{
-              color: colorScheme === 'dark' ? '#FFFFFF' : '#000000',
-            }}>
-              useColorScheme(): {colorScheme}
-          </Text>
-          <Button
-            title="Set to light"
-            onPress={() => Appearance.setColorScheme('light')}
-          />
-          <Button
-            title="Set to dark"
-            onPress={() => Appearance.setColorScheme('dark')}
-          />
+      <SafeAreaView>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber}
+          value={number}
+          placeholder="useless placeholder"
+          keyboardType="numeric"
+        />
       </SafeAreaView>
     </SafeAreaProvider>
   );
 };
 
-export default App;
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+
+export default TextInputExample;
