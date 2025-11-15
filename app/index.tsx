eab54db96f7ea1e01a839a932e27b0e889593c85
Sungyong An <sungyong.an@webtoonscorp.com>
2025-11-15 15:24:54 +0900

    (#10) StyleSheet: hairlineWidth

--- app/index.tsx
+++ app/index.tsx
@@ -1,54 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 const App = () => (
-  <SafeAreaProvider>
-    <SafeAreaView style={styles.container}>
-      <View style={styles.box1}>
-        <Text style={styles.text}>1</Text>
-      </View>
-      <View style={styles.box2}>
-        <Text style={styles.text}>2</Text>
-      </View>
-      <View style={styles.box3}>
-        <Text style={styles.text}>3</Text>
-      </View>
-    </SafeAreaView>
-  </SafeAreaProvider>
+  <View style={styles.container}>
+    <Text style={styles.row}>React</Text>
+    <Text style={styles.row}>Native</Text>
+  </View>
 );
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 24,
   },
-  box1: {
-    position: 'absolute',
-    top: 40,
-    left: 40,
-    width: 100,
-    height: 100,
-    backgroundColor: 'red',
-  },
-  box2: {
-    ...StyleSheet.absoluteFillObject,
-    top: 120,
-    left: 50,
-    width: 100,
-    height: 100,
-    backgroundColor: 'blue',
-  },
-  box3: {
-    ...StyleSheet.absoluteFillObject,
-    top: 120,
-    left: 120,
-    width: 100,
-    height: 100,
-    backgroundColor: 'green',
-  },
-  text: {
-    color: '#FFF',
-    fontSize: 80,
+  row: {
+    padding: 4,
+    borderBottomColor: 'red',
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
 
