0a41959d95f0e06e1eb6fc1d2d1fb5add69a139c
Sungyong An <sungyong.an@webtoonscorp.com>
2025-11-15 15:44:16 +0900

    (#11) Appearance

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, useColorScheme } from 'react-native';
+import { Appearance, Button, Text, useColorScheme } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 const App = () => {
   const colorScheme = useColorScheme();
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-        <Text>useColorScheme(): {colorScheme}</Text>
+      <SafeAreaView
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: colorScheme === 'dark' ? '#000000' : '#FFFFFF',
+        }}>
+          <Text
+            style={{
+              color: colorScheme === 'dark' ? '#FFFFFF' : '#000000',
+            }}>
+              useColorScheme(): {colorScheme}
+          </Text>
+          <Button
+            title="Set to light"
+            onPress={() => Appearance.setColorScheme('light')}
+          />
+          <Button
+            title="Set to dark"
+            onPress={() => Appearance.setColorScheme('dark')}
+          />
       </SafeAreaView>
     </SafeAreaProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
